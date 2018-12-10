2f18fecb5b8cff620a111a38db8431a6de9cbdb6
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-12-10 11:42:41 +0200

    Removed superfluous

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,4 @@
 (function ticTacToe() {
-  const chips = document.querySelector('.chips');
-  const library = ['apple', 'lemon', 'Lime', 'Orange', 'starawberry'];
-
   const createElement = (tag, parrentElem, elemClass) => {
     const createdElem = document.createElement(tag);
     parrentElem.appendChild(createdElem);
@@ -11,7 +8,7 @@
 
   const checkLibrary = (words, lib) => {
     const showArr = [];
-    let regex = new RegExp(`^${words}`, 'i');
+    const regex = new RegExp(`^${words}`, 'i');
     lib.forEach((libItem) => {
       if (regex.test(libItem)) {
         showArr.push(libItem);
@@ -20,44 +17,55 @@
     return showArr;
   };
 
-  const showHints = (hintsArr) => {
-    const dropList = document.querySelector('.chips__list');
+  const showHints = (library, dropList) => {
     dropList.innerHTML = '';
-    hintsArr.forEach((hint) => {
+    library.forEach((hint) => {
       const dropItem = createElement('li', dropList, 'chips__drop-item');
       dropItem.innerHTML = hint;
     });
   };
 
-  const inputEventListener = (event) => {
-    showHints(checkLibrary(event.target.value, library));
+  const setChip = (event, input, chipsContainer, chipsList) => {
+    const chip = createElement('div', chipsContainer, 'chips__chip');
+    const chipText = createElement('span', chip);
+    event.keyCode === 13 ? chipText.innerHTML = input.value : chipText.innerHTML = event.target.innerHTML;
+    const removeChip = createElement('img', chip, 'chips__remove-chip');
+    removeChip.setAttribute('src', 'images/remove.png');
+    input.value = '';
+    chipsList.innerHTML = '';
   };
 
-  const chipsEventListener = (event) => {
-    const chipsContainer = document.querySelector('.chips__container');
-    const chipsList = document.querySelector('.chips__list');
-    const input = document.querySelector('.chips__input');
+  const inputEventListener = (event, library, dropList, input, chipsContainer, chipsList) => {
+    showHints(checkLibrary(event.target.value, library), dropList);
+    if (event.keyCode === 13) {
+      setChip(event, input, chipsContainer, chipsList);
+    }
+  };
+
+  const chipsEventListener = (event, chipsContainer, library, dropList, input, chipsList) => {
     if (/drop-item/.test(event.target.className)) {
-      input.value = '';
-      const chip = createElement('div', chipsContainer, 'chips__chip');
-      const chipText = createElement('span', chip);
-      chipText.innerHTML = event.target.innerHTML;
-      const removeChip = createElement('img', chip, 'chips__remove-chip');
-      removeChip.setAttribute('src', 'images/remove.png');
-      chipsList.innerHTML = '';
+      setChip(event, input, chipsContainer, chipsList);
     } else if (/chips__remove-chip/.test(event.target.className)) {
       event.target.parentNode.remove();
     } else if (/chips__input/.test(event.target.className)) {
-      showHints(library);
+      showHints(library, dropList);
     }
   };
 
   const chipsInit = () => {
+    const library = ['apple', 'lemon', 'Lime', 'Orange', 'starawberry'];
+    const chips = document.querySelector('.chips');
     const chipsContainer = createElement('div', chips, 'chips__container');
     const input = createElement('input', chips, 'chips__input');
     const dropList = createElement('ul', chips, 'chips__list');
-    input.addEventListener('keyup', inputEventListener);
-    chips.addEventListener('click', chipsEventListener);
+    const chipsList = document.querySelector('.chips__list');
+
+    input.addEventListener('keyup', (event) => {
+      inputEventListener(event, library, dropList, input, chipsContainer, chipsList);
+    });
+    chips.addEventListener('click', (event) => {
+      chipsEventListener(event, chipsContainer, library, dropList, input, chipsList);
+    });
   };
 
   chipsInit();
